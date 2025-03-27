08889976c023b4e234df6a3ea8c05be17103ebcd
EvelynFluttershy <evelyn.fluttershy@gmail.com>
2025-03-27 19:25:32 +0100

    Formattering och kommentarer

--- functions.py
+++ functions.py
@@ -1,62 +1,51 @@
-import os #för systym functionar
-import msvcrt # ett micarosoft functionar
-def clear():# toma terminal från texten
-    os.system("cls")
-def getch():#hämtar knapar från tangbordet
-    return msvcrt.getch ()
-def xy(array,x,y,value):# vänder på x/y för inmatning till array
-    array[y][x] = value
-def text_left_right (width,left,right):
-    left_len = len(left)
-    right_len = len(right)
-    spacing = width-left_len-right_len
-    return left+" "*spacing+right
-       
-W=9
-C=4
+import os # bibliotek för systemfunktioner
+import msvcrt # bibliotek för Microsoft-funktioner
+def clear(): # funktion som tömmer terminalen från all text
+	os.system("cls")
+def getch(): # funktion som väntar på första tangenttryckning
+	return msvcrt.getch()
+def xy(array, x, y, value): # funktion som vänder på x/y för inmatning till array
+	array[y][x] = value
+def text_left_right(width, left, right): # funktion som placerar två strängar till vänster och höger baserad på en viss bredd
+	left_len = len(left)
+	right_len = len(right)
+	spacing = width - left_len - right_len
+	return left + " "*spacing + right
 
-#skapa funkionar för att rita ut spelplan
-def print_map(player,terrain,pickup):
-    x=player.get_x () 
-    y=player.get_y () 
-    map=[  
-        ["  ","  ","  ","  ","  ","  ","  ","  ","  "],
-        ["  ","  ","  ","  ","  ","  ","  ","  ","  "],
-        ["  ","  ","  ","  ","  ","  ","  ","  ","  "],
-        ["  ","  ","  ","  ","  ","  ","  ","  ","  "],
-        ["  ","  ","  ","  ","  ","  ","  ","  ","  "],
-        ["  ","  ","  ","  ","  ","  ","  ","  ","  "],
-        ["  ","  ","  ","  ","  ","  ","  ","  ","  "],
-        ["  ","  ","  ","  ","  ","  ","  ","  ","  "],
-        ["  ","  ","  ","  ","  ","  ","  ","  ","  "]
-     ] 
-    
-    for t in terrain:
-        tx= t.get_x()-x+C#
-        ty= t.get_y()-y+C
-        if tx>=0 and tx< W and  ty>=0 and ty< W:
-          xy(map,tx,ty,t.get_emoji())
+W = 9 # antalet rutor i bredd man kan se
+C = 4 # positionen i mitten där spelaren placeras
 
+def print_map(player, terrain, pickup): # funktion som ritar ut hela spelplanen samt text runt
+	x = player.get_x() 
+	y = player.get_y() 
+	map = [
+		["  ","  ","  ","  ","  ","  ","  ","  ","  "], 
+		["  ","  ","  ","  ","  ","  ","  ","  ","  "], 
+		["  ","  ","  ","  ","  ","  ","  ","  ","  "], 
+		["  ","  ","  ","  ","  ","  ","  ","  ","  "], 
+		["  ","  ","  ","  ","  ","  ","  ","  ","  "], 
+		["  ","  ","  ","  ","  ","  ","  ","  ","  "], 
+		["  ","  ","  ","  ","  ","  ","  ","  ","  "], 
+		["  ","  ","  ","  ","  ","  ","  ","  ","  "], 
+		["  ","  ","  ","  ","  ","  ","  ","  ","  "]
+	]
+	
+	for t in terrain:
+		tx = t.get_x() - x + C #
+		ty = t.get_y() - y + C
+		if tx> = 0 and tx< W and ty> = 0 and ty< W:
+			xy(map, tx, ty, t.get_emoji())
+	for t in pickup:
+		tx = t.get_x() - x + C #
+		ty = t.get_y() - y + C
+		if tx> = 0 and tx< W and ty> = 0 and ty< W:
+			xy(map, tx, ty, t.get_emoji())
+	map[C][C] = "🔴"
 
-    for t in pickup:
-        tx= t.get_x()-x+C#
-        ty= t.get_y()-y+C
-        if tx>=0 and tx< W and  ty>=0 and ty< W:
-          xy(map,tx,ty,t.get_emoji())
-
-
-
-
-
-    map[C][C]= "🔴"
-
-    print(text_left_right(22,player.get_name().upper(),str(player.get_health())+"/"+str(player.get_max_health())))
-    print("█"*(W*2+4))
-    for i in range (W):
-        print("██"+"".join(map[i])+"██")
-    print("█"*(W*2+4))
-    print(text_left_right(22,f"LEVEL {player.get_level()}",f"{player.get_gold()} G"))
- 
-    
-
-   
+	print(text_left_right(22, player.get_name().upper(), str(player.get_health())+"/"+str(player.get_max_health())))
+	print("█"*(W*2+4))
+	for i in range(W):
+		print("██"+"".join(map[i])+"██")
+	print("█"*(W*2+4))
+	print(text_left_right(22, f"LEVEL {player.get_level()}", f"{player.get_gold()} G"))
+# slutet på def print_map()
--- main.py
+++ main.py
@@ -1,35 +1,33 @@
 from classes import *
 from functions import *
-clear () # för att tomma skärmen varja gång innan kör programmet.
+
+# här initieras värden #
+clear() # för att tömma skärmen när spelet först startar
 print(" name your character:")
-var= input()
-player = Player(50,50,var)
+var = input()
+player = Player(50, 50, var) # skapar ett objekt för spelaren
 terrain = [
-   Terrain(45,51,"tree"),
-   Terrain(47,52,"tree"),
-   Terrain(48,44,"tree"),
-   Terrain(49,55,"tree"),
-   Terrain(53,46,"tree"),
+	Terrain(45, 51, "tree"),
+	Terrain(47, 52, "tree"),
+	Terrain(48, 44, "tree"),
+	Terrain(49, 55, "tree"),
+	Terrain(53, 46, "tree")
 ]
-pickup=[
-  Pickup(52,46,"crown",10),
-  Pickup(48,56,"crown",10)
-
-
+pickup = [
+	Pickup(52, 46, "crown", 10),
+	Pickup(48, 56, "crown", 10)
 ]
 
-# kör spelet
-while True :
-    clear()
-    print_map(player,terrain,pickup)
-    var = getch()#likna input 
-    if var == b"w":
-      player.move(0,-1,terrain,pickup) 
-    elif var == b"s":
-      player.move(0,1,terrain,pickup) 
-    elif var == b"a":
-      player.move(-1,0,terrain,pickup) 
-    elif var == b"d":
-      player.move(1,0,terrain,pickup)
- #     
-
+# här körs spelet
+while True:
+	clear()
+	print_map(player, terrain, pickup)
+	var = getch() # vänta på att en knapp trycks
+	if var == b"w": # upp
+		player.move( 0, -1, terrain, pickup) 
+	elif var == b"s": # ned
+		player.move( 0,  1, terrain, pickup) 
+	elif var == b"a": # vänster
+		player.move(-1,  0, terrain, pickup) 
+	elif var == b"d": # höger
+		player.move( 1,  0, terrain, pickup)
